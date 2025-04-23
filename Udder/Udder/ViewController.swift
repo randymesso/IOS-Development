b2d3186fe5ec62a118e37f10429eedf17bb52ac4
Randy Messo <65696284+randymesso@users.noreply.github.com>
2025-04-22 23:09:20 -0500

    Add files via upload

---
+++ Udder/Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Randy Messo on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController 
+{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
